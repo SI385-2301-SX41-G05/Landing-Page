fbcc100a269511a6a1e432c540b64a1e1be1b3aa
Carlos Martinez <u202010153@upc.edu.pe>
2023-06-09 20:51:03 -0500

    Se modifico el sistema de navegacion
    -Se modifico el sistema de navegacion.
    -se hizo compatible con mobile.
    -se implemento js para la barra de navegacion.

--- Js/scripts.js
+++ Js/scripts.js
@@ -5,3 +5,12 @@ document.getElementById("my-button").addEventListener("click",function(){
 document.getElementBytype("button").addEventListener("click",function(){
     alert("Hola mundo");
 })
+
+
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%";
+}
