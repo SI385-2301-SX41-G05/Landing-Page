13c3434317b231c48a2dcb96ff2cf006b3aba5d1
Vilder Sandoval <Luisvilders@gmail.com>
2023-06-09 20:38:20 -0500

    otra
    Co-Authored-By: Carlos Martinez <90540178+MbadMC@users.noreply.github.com>
    Co-Authored-By: Romelmori <129351717+Romelmori@users.noreply.github.com>
    Co-Authored-By: Italo18-User <134071838+Italo18-User@users.noreply.github.com>
    Co-Authored-By: GamboaBrayan <118092973+GamboaBrayan@users.noreply.github.com>

---
+++ Js/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("my-button").addEventListener("click",function(){
+    alert("Hola mundo");
+})
+
+document.getElementBytype("button").addEventListener("click",function(){
+    alert("Hola mundo");
+})
