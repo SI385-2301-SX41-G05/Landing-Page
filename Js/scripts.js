fe3385d2161f8914bdd1f5564f3d1aa9d9ff691d
Vilder Sandoval <Luisvilders@gmail.com>
2023-06-16 00:36:36 -0500

    d

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,5 +1,5 @@
 document.getElementById("my-button").addEventListener("click",function(){
-    alert("Hola mundo");
+    alert("La alimentación y la nutrición son términos que tienen mucha relación entre sí. Iniciamos mencionando que la alimentación parte de ser una necesidad básica de ingerir alimentos para satisfacer nuestros impulsos de apetito. Asimismo, se trata de un conjunto de acciones en las qué ingresamos nutrientes a nuestro organismo, de manera voluntaria y consciente. Además, mediante la alimentación podemos ver reflejados nuestros gustos, creencias religiosas y culturales.");
 })
 
 document.getElementBytype("button").addEventListener("click",function(){
