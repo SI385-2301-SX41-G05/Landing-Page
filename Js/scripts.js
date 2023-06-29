29c3dc0c80fab97f63d45a77be72d404eb2290cd
Vilder Sandoval <Luisvilders@gmail.com>
2023-06-29 12:57:28 -0500

    Mejora 1

--- Js/scripts.js
+++ Js/scripts.js
@@ -2,10 +2,6 @@ document.getElementById("my-button").addEventListener("click",function(){
     alert("La alimentación y la nutrición son términos que tienen mucha relación entre sí. Iniciamos mencionando que la alimentación parte de ser una necesidad básica de ingerir alimentos para satisfacer nuestros impulsos de apetito. Asimismo, se trata de un conjunto de acciones en las qué ingresamos nutrientes a nuestro organismo, de manera voluntaria y consciente. Además, mediante la alimentación podemos ver reflejados nuestros gustos, creencias religiosas y culturales.");
 })
 
-document.getElementBytype("button").addEventListener("click",function(){
-    alert("Hola mundo");
-})
-
 
 function openNav(){
     document.getElementById("mobile-menu").style.width = "100%";
@@ -15,3 +11,16 @@ function closeNav(){
     document.getElementById("mobile-menu").style.width = "0%";
     
 }
+
+
+
+document.getElementById("My-joven").addEventListener("click", function() {
+        alert("Jóvenes de 18-30 años con estilo de vida activo e interesados en aprender sobre nutrición equilibrada y hábitos saludables. Que buscan opciones rápidas y prácticas para sus comidas teniendo como objetivo dieta balanceada y mejorar el rendimiento físico. Además, ofrecemos planes de comidas, recetas saludables y consejos de nutrición.");
+    });
+
+document.getElementById("My-profe").addEventListener("click", function() {
+        alert("Profesionales con conocimiento y experiencia en nutrición.Interesados en estar actualizados y colaborar con otros profesionales.Teniendo como objetivo promover alimentación saludable y bienestar.Con la necesidad de encontrar información científica, recursos educativos,herramientas de cálculo y comunicación con otros expertos. Haz resumen corto");
+    });
+
+
+
