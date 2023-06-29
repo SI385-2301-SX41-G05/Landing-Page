8ad72e1d67b83fd62ebe2a6d65e5fea55058f4e0
Vilder Sandoval <Luisvilders@gmail.com>
2023-06-29 13:56:11 -0500

    s

--- Js/scripts.js
+++ Js/scripts.js
@@ -6,7 +6,9 @@ document.getElementById("my-button").addEventListener("click",function(){
 function openNav(){
     document.getElementById("mobile-menu").style.width = "100%";
 }
-
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
 function closeNav(){
     document.getElementById("mobile-menu").style.width = "0%";
     
